e078a8810ff0c7092eae6d8fb63e2684fe529bbc
Eugene Bekker <ebekker@users.noreply.github.com>
2019-06-19 11:17:10 -0400

    updating published GH Pages

--- docs/js/blazor-boot-progress.js
+++ docs/js/blazor-boot-progress.js
@@ -1,9 +1,14 @@
+
+// Parse the URL params to control functionality below
+var urlParams = new URLSearchParams(window.location.search);
+
 // This should be set to max number of DLL assemblies expected
 // to be loaded at boot up which can be discovered during
 // development by examining the DLL loaded in the JS console
-window.blazor_boot_dll_max = 25;
-window.blazor_boot_dll_min = 0;
-window.blazor_boot_dll_val = 0;
+window.blazor_boot_dll_max = urlParams.has("dll_max") ? parseInt(urlParams.get("dll_max")) : 25;
+window.blazor_boot_dll_min = urlParams.has("dll_min") ? parseInt(urlParams.get("dll_min")) : 0;
+window.blazor_boot_dll_val = urlParams.has("dll_val") ? parseInt(urlParams.get("dll_val")) : 0;
+window.blazor_boot_dll_msg = urlParams.has("dll_msg") ? urlParams.get("dll_msg") : "Loading application...";
 
 window.blazor_boot_update_progress = function (min, max, val, msg) {
     var msgElm = document.getElementById("bootMessage");
@@ -21,6 +26,14 @@ window.blazor_boot_update_progress = function (min, max, val, msg) {
     barElm.setAttribute("aria-valuemax", max);
     barElm.setAttribute("aria-valuenow", val);
 }
+
+// Update static progress elements with initial state
+window.blazor_boot_update_progress(
+    window.blazor_boot_dll_min,
+    window.blazor_boot_dll_max,
+    window.blazor_boot_dll_val,
+    window.blazor_boot_dll_msg
+);
 
 XMLHttpRequest.prototype.orig_open = XMLHttpRequest.prototype.open;
 XMLHttpRequest.prototype.open = function (method, url, async) {
@@ -47,16 +60,29 @@ XMLHttpRequest.prototype.open = function (method, url, async) {
     this.orig_open(method, url, async);
 }
 
-// Artificial progressive delay for dev/test
-window.blazor_boot_sendDelay = 1;
-XMLHttpRequest.prototype.orig_send = XMLHttpRequest.prototype.send;
-XMLHttpRequest.prototype.send = function () {
-    if (this.is_dll && window.blazor_boot_sendDelay > 0) {
-        setTimeout(function (xhr) {
+
+
+// Artificial progressive delay and abort for dev/test
+window.blazor_boot_xhr_send_delay_count = 1;
+window.blazor_boot_xhr_send_abort_count =  urlParams.has("xhr_send_abort_count")
+? parseInt(urlParams.get("xhr_send_abort_count")) : 0;
+window.blazor_boot_xhr_send_delay_factor = urlParams.has("xhr_send_delay_factor")
+    ? parseInt(urlParams.get("xhr_send_delay_factor")) : 0;
+
+if (window.blazor_boot_xhr_send_delay_factor) {
+    XMLHttpRequest.prototype.orig_send = XMLHttpRequest.prototype.send;
+    XMLHttpRequest.prototype.send = function () {
+        if (window.blazor_boot_xhr_send_abort_count > 0
+            && window.blazor_boot_xhr_send_delay_count >= window.blazor_boot_xhr_send_abort_count)
+            return;
+
+        if (this.is_dll && window.blazor_boot_xhr_send_delay_count > 0) {
+            var timeout = ++window.blazor_boot_xhr_send_delay_count
+                * window.blazor_boot_xhr_send_delay_factor;
+            setTimeout(function (xhr) { xhr.orig_send(); }, timeout, this);
+        }
+        else { 
             xhr.orig_send();
-        }, ++window.blazor_boot_sendDelay * 200, this);
-    }
-    else { 
-        xhr.orig_send();
+        }
     }
 }
